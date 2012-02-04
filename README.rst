67e3f97f1476a82bd22cbc3ef7f266d97af70a7e
AnonToolBox Dev <anontoolbox@yahoo.com>
2012-02-04 09:35:29 +0100

    Initialize AnonToolBox
    Signed-off-by: AnonToolBox Dev <anontoolbox@yahoo.com>

---
+++ README.rst
@@ -0,0 +1,34 @@
+AnonToolBox - Easy to use tools for #Anons
+==========================================
+
+.. toctree::
+   :maxdepth: 3
+
+The AnonToolBox is NOT a hacker's toolbox!
+It should help people without deep skills to prepare,
+upload and spread infos! It should support free speech!
+
+If you like to support or join the AnonToolBox Team,
+send us your ideas for tools, code snippets or write
+easy to understand documentation.
+
+To get the AnonToolBox working, only a standard
+Python2.6 (or newer) runtime should be available.
+
+A local webserver supports with you with simple
+to understand documentation/howtos.
+
+
+Quickstart
+==========
+
+Download and buildout AnonToolBox
+
+* git co PATH_TO_GITHUB_REPO
+* cd anontoolbox
+* python bootstrap.py
+* bin/buildout
+
+After bootstrapping, you can find all tools in the bin/ folder.
+
+Feel free to send us your feedback!
---
+++ setup.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+
+# import std
+import os
+from os import path
+import re
+import sys
+# import third party
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    try:
+        from ez_setup import use_setuptools
+    except ImportError:
+        print "can't find ez_setup"
+        print "try: wget http://peak.telecommunity.com/dist/ez_setup.py"
+        sys.exit(1)
+    use_setuptools()
+    from setuptools import setup, find_packages
+
+if sys.version_info < (2, 6):
+    print "We don't support old Python releases!"
+    print "Come on, upgrade at least to Python2.6 :)"
+    sys.exit(1)
+
+sys.path.append(path.join(path.dirname(__file__), 'src'))
+
+__version__ = '0.1'
+__author__  = 'AnonToolBox Crew'
+__author_email__ = 'anontoolbox@yahoo.com'
+__package_data__ = [
+    'README.rst',
+    'Changelog',
+    'License',
+    'Makefile',
+    'buildout'
+]
+
+
+setup(
+    name = 'anontoolbox',
+    version = __version__,
+    author = __author__,
+    author_email = __author_email__,
+    description = open('README.rst').read().split('\n')[0],
+    long_description = open('README.rst').read(),
+    url = 'http://anonnewsde.tumblr.com',
+    platforms = ["Python >= 2.6"],
+    license = 'Apache Software License',
+    package_dir = {'': 'src'},
+    packages = find_packages('src'),
+    package_data = {'': __package_data__},
+    include_package_data = True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    entry_points = {
+        'console_scripts': [
+        ]
+    },
+    install_requires=[
+          'setuptools',
+    ]
+)
+
